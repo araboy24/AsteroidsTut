949cd075c6b2e73ad77d168c3b77961630b8e6dd
Eid Zaben <eid.s.zaben-1@ou.edu>
2020-12-02 13:41:08 +0200

    part 2 bullets

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ class Player(object):
         self.rotatedRect.center = (self.x, self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cosine + self.w//2, self.y - self.sine * self.h//2)
+        self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
 
     def turnRight(self):
         self.angle -= 5
@@ -55,7 +55,7 @@ class Player(object):
         self.rotatedRect.center = (self.x, self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cosine + self.w//2, self.y - self.sine * self.h//2)
+        self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
 
     def moveForward(self):
         self.x += self.cosine * 6
@@ -65,19 +65,61 @@ class Player(object):
         self.rotatedRect.center = (self.x, self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
-        self.head = (self.x + self.cosine + self.w // 2, self.y - self.sine * self.h // 2)
+        self.head = (self.x + self.cosine * self.w // 2, self.y - self.sine * self.h // 2)
+
+    def updateLocation(self):
+        if self.x > sw + 50:
+            self.x = 0
+        elif self.x < 0 - self.w:
+            self.x = sw
+        elif self.y < -50:
+            self.y = sh
+        elif self.y > sh + 50:
+            self.y = 0
+
+class Bullet(object):
+    def __init__(self):
+        self.point = player.head
+        self.x, self.y = self.point
+        self.w = 4
+        self.h = 4
+        self.c = player.cosine
+        self.s = player.sine
+        self.xv = self.c * 10
+        self.yv = self.s * 10
+
+    def move(self):
+        self.x += self.xv
+        self.y -= self.yv
+
+    def draw(self, win):
+        pygame.draw.rect(win, (255, 255, 255), [self.x, self.y, self.w, self.h])
+
+    def checkOffScreen(self):
+        if self.x < -50 or self.x > sw or self.y > sh or self.y < -50:
+            return True
 
 def redrawGameWindow():
     win.blit(bg, (0,0))
     player.draw(win)
+    for b in playerBullets:
+        b.draw(win)
     pygame.display.update()
 
 
 player = Player()
+playerBullets = []
 run = True
 while run:
     clock.tick(60)
     if not gameover:
+        player.updateLocation()
+        for b in playerBullets:
+            b.move()
+            if b.checkOffScreen():
+                playerBullets.pop(playerBullets.index(b))
+
+
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             player.turnLeft()
@@ -89,6 +131,10 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if not gameover:
+                    playerBullets.append(Bullet())
 
     redrawGameWindow()
 pygame.quit()
